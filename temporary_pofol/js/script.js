d1cfa0059d830331864f67a384405dec6f56ecaa
bigblackbean <41283756+bigblackbean@users.noreply.github.com>
2020-12-28 19:11:03 +0900

    menu

--- temporary_pofol/js/script.js
+++ temporary_pofol/js/script.js
@@ -1,15 +1,19 @@
 
-// nav button
-const navBtn = document.getElementById('navBtn');
+// Navigation button
+const navBtn = document.querySelector('#navBtn');
+const navMenu = document.querySelector('.navi');
 navBtn.addEventListener('click', function(){
   const hasClass = navBtn.classList.contains('on');
   if (hasClass) {
     navBtn.classList.remove('on');
+    navMenu.style.left = '-100%';
   }else {
     navBtn.classList.add('on');
+    navMenu.style.left = '0%';
   }
 });
 
+// Layer pop up
 const popClose = document.getElementById('popClose');
 popClose.addEventListener('click', function(e){
   e.preventDefault();
