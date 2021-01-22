8b1ba98289c2b28e44f9470421d153cc8ed6c387
bigblackbean <41283756+bigblackbean@users.noreply.github.com>
2021-01-22 15:57:54 +0900

    sub page

--- temporary_pofol/js/script.js
+++ temporary_pofol/js/script.js
@@ -2,31 +2,36 @@
 // Navigation button
 const navBtn = document.querySelector('#navBtn');
 const navMenu = document.querySelector('.navi');
-navBtn.addEventListener('click', function(){
+navBtn.addEventListener('click', function naviToggle(){
   const hasClass = navBtn.classList.contains('on');
   if (hasClass) {
     navBtn.classList.remove('on');
-    navMenu.style.left = '-100%';
+    navMenu.style.right = '-100%';
   }else {
     navBtn.classList.add('on');
-    navMenu.style.left = '0%';
+    navMenu.style.right = '0%';
   }
 });
 
 // Layer pop up
-const popClose = document.getElementById('popClose');
-popClose.addEventListener('click', function(e){
-  e.preventDefault();
-  document.querySelector('.pop').style.opacity = 0;
-});
+// const popClose = document.getElementById('popClose');
+// popClose.addEventListener('click', function(e){
+//   e.preventDefault();
+//   document.querySelector('.pop').style.opacity = 0;
+// });
 
 // About Me Flip
+const aboutMeContIdx = document.getElementsByClassName('about-cont');
 const aboutMeCont = document.querySelector('.about-cont');
+const aboutMeContLength = aboutMeContIdx.length;
 aboutMeCont.addEventListener('click', function(){
-  const hasClass = aboutMeCont.classList.contains('flip');
-  if (hasClass) {
-    aboutMeCont.classList.remove('flip');
-  }else {
-    aboutMeCont.classList.add('flip');
+  console.log(this);
+  for(var i = 0; i < aboutMeContLength; i++) {
+    const hasClass = this[i].classList.contains('flip');
+    if (hasClass) {
+      aboutMeCont.classList.remove('flip');
+    }else {
+      aboutMeCont.classList.add('flip');
+    }
   }
 });
